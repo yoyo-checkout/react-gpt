fbade44e480dc58e3302c7ca94ed09333c72d131
John <john@unnotech.com>
2024-04-14 15:50:12 +0800

    chore: add path alias

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -19,7 +19,14 @@ module.exports = {
     'import/resolver': {
       node: {
         paths: ['src'],
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        extensions: ['.js', '.jsx', '.ts', '.d.ts', '.tsx'],
+      },
+      typescript: {
+        project: './tsconfig.json',
+      },
+      alias: {
+        map: [['@', path.resolve(__dirname, './src')]],
+        extensions: ['.js', '.jsx', '.ts', '.d.ts', '.tsx'],
       },
     },
   },
