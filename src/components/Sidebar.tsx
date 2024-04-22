2eb7b3915d78129eb77663c0dabf6c596ea0c0a6
John <stem597sag445@gmail.com>
2024-04-22 16:19:50 +0800

    feat: rename chat

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,15 @@
 import { Popover } from '@headlessui/react'
 import dayjs from 'dayjs'
 import { groupBy, orderBy } from 'lodash-es'
-import { FC, MouseEvent as ReactMouseEvent, useMemo, useRef, useState } from 'react'
+import {
+  FC,
+  ChangeEvent as ReactChangeEvent,
+  KeyboardEvent as ReactKeyboardEvent,
+  MouseEvent as ReactMouseEvent,
+  useMemo,
+  useRef,
+  useState,
+} from 'react'
 import { usePopper } from 'react-popper'
 import { NavLink } from 'react-router-dom'
 import { useClickAway } from 'react-use'
@@ -36,19 +44,47 @@ const ListItem: FC<ListItemProps> = ({ chat }) => {
     updateChat({ ...chat, status: 'archived' })
   }
 
-  const handleClickAction = (e: ReactMouseEvent, action: 'share' | 'rename' | 'delete') => {
+  const nameEditingInputRef = useRef<HTMLInputElement | null>(null)
+  const [isEditing, setIsEditing] = useState(false)
+  const [nameEditing, setNameEditing] = useState(chat.name)
+
+  const handleChangeNameEditing = (e: ReactChangeEvent<HTMLInputElement>) => {
+    setNameEditing(() => e.target.value)
+  }
+  const handleKeyDownNameEditing = (e: ReactKeyboardEvent<HTMLInputElement>) => {
+    if (e.code === 'Enter') {
+      updateChat({ ...chat, name: nameEditing })
+      setIsEditing(false)
+      setNameEditing('')
+    }
+  }
+  const renameChat = () => {
+    setIsEditing(true)
+    setTimeout(() => {
+      nameEditingInputRef.current?.focus()
+    }, 0)
+  }
+
+  const handleClickAction = async (
+    e: ReactMouseEvent,
+    action: 'share' | 'rename' | 'delete',
+    closePanel: () => void,
+  ) => {
     e.preventDefault()
     e.stopPropagation()
 
     switch (action) {
       case 'share':
-      case 'rename':
         mittBus.emit('openFeatureDisabledDialog')
+        break
+      case 'rename':
+        renameChat()
         break
       case 'delete':
         deleteChat(chat.id)
         break
     }
+    closePanel()
   }
 
   return (
@@ -56,7 +92,7 @@ const ListItem: FC<ListItemProps> = ({ chat }) => {
       <div className="group rounded-lg active:opacity-90 hover:bg-token-sidebar-surface-secondary">
         <NavLink
           to={`/c/${chat.id}`}
-          className="flex items-center gap-2 p-2"
+          className="relative flex items-center gap-2 p-2"
           onClick={() => breakpoint === 'sm' && mittBus.emit('toggleSidebar')}
         >
           <div className="grow overflow-hidden whitespace-nowrap">{chat.name}</div>
@@ -71,73 +107,75 @@ const ListItem: FC<ListItemProps> = ({ chat }) => {
               ref={setPopperElement}
               style={styles.popper}
               {...attributes.popper}
-              className="min-w-max will-change-transform"
+              className="z-[1] min-w-max will-change-transform"
             >
-              <div className="popover min-w-[200px] max-w-xs rounded-lg border border-token-border-light bg-token-main-surface-primary shadow-lg outline-none">
-                <div
-                  className="flex items-center gap-2 m-1.5 rounded p-2.5 text-sm cursor-pointer focus:ring-0 hover:bg-token-main-surface-secondary radix-disabled:pointer-events-none radix-disabled:opacity-50 group"
-                  onClick={(e) => handleClickAction(e, 'share')}
-                >
-                  <svg
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="icon-md"
+              {({ close }) => (
+                <div className="popover min-w-[200px] max-w-xs rounded-lg border border-token-border-light bg-token-main-surface-primary shadow-lg outline-none">
+                  <div
+                    className="flex items-center gap-2 m-1.5 rounded p-2.5 text-sm cursor-pointer focus:ring-0 hover:bg-token-main-surface-secondary radix-disabled:pointer-events-none radix-disabled:opacity-50 group"
+                    onClick={(e) => handleClickAction(e, 'share', close)}
                   >
-                    <path
-                      fillRule="evenodd"
-                      clipRule="evenodd"
-                      d="M11.2929 2.29289C11.6834 1.90237 12.3166 1.90237 12.7071 2.29289L16.7071 6.29289C17.0976 6.68342 17.0976 7.31658 16.7071 7.70711C16.3166 8.09763 15.6834 8.09763 15.2929 7.70711L13 5.41421V14C13 14.5523 12.5523 15 12 15C11.4477 15 11 14.5523 11 14V5.41421L8.70711 7.70711C8.31658 8.09763 7.68342 8.09763 7.29289 7.70711C6.90237 7.31658 6.90237 6.68342 7.29289 6.29289L11.2929 2.29289ZM4 13C4.55228 13 5 13.4477 5 14V18C5 18.5523 5.44772 19 6 19H18C18.5523 19 19 18.5523 19 18V14C19 13.4477 19.4477 13 20 13C20.5523 13 21 13.4477 21 14V18C21 19.6569 19.6569 21 18 21H6C4.34315 21 3 19.6569 3 18V14C3 13.4477 3.44772 13 4 13Z"
-                      fill="currentColor"
-                    ></path>
-                  </svg>
-                  分享
-                </div>
-                <div
-                  className="flex items-center gap-2 m-1.5 rounded p-2.5 text-sm cursor-pointer focus:ring-0 hover:bg-token-main-surface-secondary radix-disabled:pointer-events-none radix-disabled:opacity-50 group"
-                  onClick={(e) => handleClickAction(e, 'rename')}
-                >
-                  <svg
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="icon-md"
+                    <svg
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="icon-md"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M11.2929 2.29289C11.6834 1.90237 12.3166 1.90237 12.7071 2.29289L16.7071 6.29289C17.0976 6.68342 17.0976 7.31658 16.7071 7.70711C16.3166 8.09763 15.6834 8.09763 15.2929 7.70711L13 5.41421V14C13 14.5523 12.5523 15 12 15C11.4477 15 11 14.5523 11 14V5.41421L8.70711 7.70711C8.31658 8.09763 7.68342 8.09763 7.29289 7.70711C6.90237 7.31658 6.90237 6.68342 7.29289 6.29289L11.2929 2.29289ZM4 13C4.55228 13 5 13.4477 5 14V18C5 18.5523 5.44772 19 6 19H18C18.5523 19 19 18.5523 19 18V14C19 13.4477 19.4477 13 20 13C20.5523 13 21 13.4477 21 14V18C21 19.6569 19.6569 21 18 21H6C4.34315 21 3 19.6569 3 18V14C3 13.4477 3.44772 13 4 13Z"
+                        fill="currentColor"
+                      ></path>
+                    </svg>
+                    分享
+                  </div>
+                  <div
+                    className="flex items-center gap-2 m-1.5 rounded p-2.5 text-sm cursor-pointer focus:ring-0 hover:bg-token-main-surface-secondary radix-disabled:pointer-events-none radix-disabled:opacity-50 group"
+                    onClick={(e) => handleClickAction(e, 'rename', close)}
                   >
-                    <path
-                      fillRule="evenodd"
-                      clipRule="evenodd"
-                      d="M13.2929 4.29291C15.0641 2.52167 17.9359 2.52167 19.7071 4.2929C21.4783 6.06414 21.4783 8.93588 19.7071 10.7071L18.7073 11.7069L11.1603 19.2539C10.7182 19.696 10.1489 19.989 9.53219 20.0918L4.1644 20.9864C3.84584 21.0395 3.52125 20.9355 3.29289 20.7071C3.06453 20.4788 2.96051 20.1542 3.0136 19.8356L3.90824 14.4678C4.01103 13.8511 4.30396 13.2818 4.7461 12.8397L13.2929 4.29291ZM13 7.41422L6.16031 14.2539C6.01293 14.4013 5.91529 14.591 5.88102 14.7966L5.21655 18.7835L9.20339 18.119C9.40898 18.0847 9.59872 17.9871 9.7461 17.8397L16.5858 11L13 7.41422ZM18 9.5858L14.4142 6.00001L14.7071 5.70712C15.6973 4.71693 17.3027 4.71693 18.2929 5.70712C19.2831 6.69731 19.2831 8.30272 18.2929 9.29291L18 9.5858Z"
-                      fill="currentColor"
-                    ></path>
-                  </svg>
-                  重新命名
-                </div>
-                <div
-                  className="flex items-center gap-2 m-1.5 rounded p-2.5 text-sm cursor-pointer focus:ring-0 hover:bg-token-main-surface-secondary radix-disabled:pointer-events-none radix-disabled:opacity-50 group text-red-500"
-                  onClick={(e) => handleClickAction(e, 'delete')}
-                >
-                  <svg
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="icon-md"
+                    <svg
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="icon-md"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M13.2929 4.29291C15.0641 2.52167 17.9359 2.52167 19.7071 4.2929C21.4783 6.06414 21.4783 8.93588 19.7071 10.7071L18.7073 11.7069L11.1603 19.2539C10.7182 19.696 10.1489 19.989 9.53219 20.0918L4.1644 20.9864C3.84584 21.0395 3.52125 20.9355 3.29289 20.7071C3.06453 20.4788 2.96051 20.1542 3.0136 19.8356L3.90824 14.4678C4.01103 13.8511 4.30396 13.2818 4.7461 12.8397L13.2929 4.29291ZM13 7.41422L6.16031 14.2539C6.01293 14.4013 5.91529 14.591 5.88102 14.7966L5.21655 18.7835L9.20339 18.119C9.40898 18.0847 9.59872 17.9871 9.7461 17.8397L16.5858 11L13 7.41422ZM18 9.5858L14.4142 6.00001L14.7071 5.70712C15.6973 4.71693 17.3027 4.71693 18.2929 5.70712C19.2831 6.69731 19.2831 8.30272 18.2929 9.29291L18 9.5858Z"
+                        fill="currentColor"
+                      ></path>
+                    </svg>
+                    重新命名
+                  </div>
+                  <div
+                    className="flex items-center gap-2 m-1.5 rounded p-2.5 text-sm cursor-pointer focus:ring-0 hover:bg-token-main-surface-secondary radix-disabled:pointer-events-none radix-disabled:opacity-50 group text-red-500"
+                    onClick={(e) => handleClickAction(e, 'delete', close)}
                   >
-                    <path
-                      fillRule="evenodd"
-                      clipRule="evenodd"
-                      d="M10.5555 4C10.099 4 9.70052 4.30906 9.58693 4.75114L9.29382 5.8919H14.715L14.4219 4.75114C14.3083 4.30906 13.9098 4 13.4533 4H10.5555ZM16.7799 5.8919L16.3589 4.25342C16.0182 2.92719 14.8226 2 13.4533 2H10.5555C9.18616 2 7.99062 2.92719 7.64985 4.25342L7.22886 5.8919H4C3.44772 5.8919 3 6.33961 3 6.8919C3 7.44418 3.44772 7.8919 4 7.8919H4.10069L5.31544 19.3172C5.47763 20.8427 6.76455 22 8.29863 22H15.7014C17.2354 22 18.5224 20.8427 18.6846 19.3172L19.8993 7.8919H20C20.5523 7.8919 21 7.44418 21 6.8919C21 6.33961 20.5523 5.8919 20 5.8919H16.7799ZM17.888 7.8919H6.11196L7.30423 19.1057C7.3583 19.6142 7.78727 20 8.29863 20H15.7014C16.2127 20 16.6417 19.6142 16.6958 19.1057L17.888 7.8919ZM10 10C10.5523 10 11 10.4477 11 11V16C11 16.5523 10.5523 17 10 17C9.44772 17 9 16.5523 9 16V11C9 10.4477 9.44772 10 10 10ZM14 10C14.5523 10 15 10.4477 15 11V16C15 16.5523 14.5523 17 14 17C13.4477 17 13 16.5523 13 16V11C13 10.4477 13.4477 10 14 10Z"
-                      fill="currentColor"
-                    ></path>
-                  </svg>
-                  刪除交談
+                    <svg
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="icon-md"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M10.5555 4C10.099 4 9.70052 4.30906 9.58693 4.75114L9.29382 5.8919H14.715L14.4219 4.75114C14.3083 4.30906 13.9098 4 13.4533 4H10.5555ZM16.7799 5.8919L16.3589 4.25342C16.0182 2.92719 14.8226 2 13.4533 2H10.5555C9.18616 2 7.99062 2.92719 7.64985 4.25342L7.22886 5.8919H4C3.44772 5.8919 3 6.33961 3 6.8919C3 7.44418 3.44772 7.8919 4 7.8919H4.10069L5.31544 19.3172C5.47763 20.8427 6.76455 22 8.29863 22H15.7014C17.2354 22 18.5224 20.8427 18.6846 19.3172L19.8993 7.8919H20C20.5523 7.8919 21 7.44418 21 6.8919C21 6.33961 20.5523 5.8919 20 5.8919H16.7799ZM17.888 7.8919H6.11196L7.30423 19.1057C7.3583 19.6142 7.78727 20 8.29863 20H15.7014C16.2127 20 16.6417 19.6142 16.6958 19.1057L17.888 7.8919ZM10 10C10.5523 10 11 10.4477 11 11V16C11 16.5523 10.5523 17 10 17C9.44772 17 9 16.5523 9 16V11C9 10.4477 9.44772 10 10 10ZM14 10C14.5523 10 15 10.4477 15 11V16C15 16.5523 14.5523 17 14 17C13.4477 17 13 16.5523 13 16V11C13 10.4477 13.4477 10 14 10Z"
+                        fill="currentColor"
+                      ></path>
+                    </svg>
+                    刪除交談
+                  </div>
                 </div>
-              </div>
+              )}
             </Popover.Panel>
           </Popover>
           <button
@@ -146,6 +184,18 @@ const ListItem: FC<ListItemProps> = ({ chat }) => {
           >
             <Seal className="icon-md" />
           </button>
+
+          <div
+            className={`${isEditing ? 'visible' : 'invisible'} absolute bottom-0 left-[7px] right-2 top-0 flex items-center bg-token-sidebar-surface-secondary`}
+          >
+            <input
+              ref={nameEditingInputRef}
+              className="w-full border border-token-border-light bg-transparent p-0 text-sm"
+              value={nameEditing}
+              onChange={handleChangeNameEditing}
+              onKeyDown={handleKeyDownNameEditing}
+            />
+          </div>
         </NavLink>
       </div>
     </li>
